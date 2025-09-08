f58e88d07f976cde2d5429568187043c89bc6a30
Jisang Yun <journeyofbabo@gmail.com>
2025-09-08 10:02:52 +0900

    update setup for rust

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,69 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+-- ## Jisang Setup ## --
+
+-- Nvim DAP Mappings
+map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+map(
+  "n",
+  "<Leader>dd",
+  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { desc = "Debugger set conditional breakpoint" }
+)
+map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
+-- Rust-specific Mappings
+map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+map("n", "<Leader>k", function()
+  vim.cmd.RustLsp { "hover", "actions" }
+end, { desc = "Rust hover actions" })
+map("n", "<Leader>dg", "<cmd>RustLsp debuggables<CR>", { desc = "List available debug targets" })
+map("n", "<Leader>dr", "<cmd>RustLsp debug<CR>", { desc = "Start debugging" })
+map("n", "<Leader>rr", "<cmd>RustLsp runnables<CR>", { desc = "List runnable targets" })
+map("n", "<Leader>rc", "<cmd>RustLsp run<CR>", { desc = "Run current target" })
+map("n", "<Leader>tt", "<cmd>RustLsp testables<CR>", { desc = "List testable targets" })
+map("n", "<Leader>ca", "<cmd>RustLsp codeAction<CR>", { desc = "Show code actions" })
+map("n", "<Leader>em", "<cmd>RustLsp expandMacro<CR>", { desc = "Expand macro recursively" })
+map("n", "<Leader>mu", "<cmd>RustLsp moveItem up<CR>", { desc = "Move item up" })
+map("n", "<Leader>md", "<cmd>RustLsp moveItem down<CR>", { desc = "Move item down" })
+map("n", "<Leader>ee", "<cmd>RustLsp explainError<CR>", { desc = "Explain error" })
+map("n", "<Leader>od", "<cmd>RustLsp openDocs<CR>", { desc = "Open docs.rs documentation" })
+map("n", "<Leader>pm", "<cmd>RustLsp parentModule<CR>", { desc = "Go to parent module" })
+map("n", "<Leader>ws", "<cmd>RustLsp workspaceSymbol<CR>", { desc = "Search workspace symbols" })
+map("n", "<Leader>jl", "<cmd>RustLsp joinLines<CR>", { desc = "Join lines" })
+map("n", "<Leader>sr", "<cmd>RustLsp ssr<CR>", { desc = "Structural search replace" })
+map("n", "<Leader>vg", "<cmd>RustLsp crateGraph<CR>", { desc = "View crate graph" })
+map("n", "<Leader>vs", "<cmd>RustLsp syntaxTree<CR>", { desc = "View syntax tree" })
+map("n", "<Leader>vh", "<cmd>RustLsp view hir<CR>", { desc = "View HIR" })
+map("n", "<Leader>vm", "<cmd>RustLsp view mir<CR>", { desc = "View MIR" })
+map("n", "<Leader>fc", "<cmd>RustLsp flyCheck run<CR>", { desc = "Run fly check" })
+
+-- Window Navigation
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Window left" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "Window right" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "Window down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "Window up" })
+
+-- LSP Mappings
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "LSP definition" })
+map("n", "gdv", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", { desc = "LSP definition (vertical split)" })
+map(
+  "n",
+  "gdh",
+  "<cmd>belowright split | lua vim.lsp.buf.definition()<CR>",
+  { desc = "LSP definition (horizontal split)" }
+)
+
+-- Cursor Movement
+map("n", "<leader>lj", "10jzz", { desc = "Move cursor up by 10 lines" })
+map("n", "<leader>lk", "10kzz", { desc = "Move cursor down by 10 lines" })
+
+
+-- ## Jisang Setup End ## --
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,153 @@ return {
     end,
   },
 
+  -- ## Jisang Setup ## --
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        -- "lua-language-server",
+        "pyright",
+        "black",
+        "debugpy",
+        "mypy",
+        -- "ruff",  -- Removed temporarily due to loading issues
+        "clangd",
+        "clang-format",
+        "codelldb",
+      },
+    },
+  },
+
+  -- Rust Configuration
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ft = "rust",
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Let rustaceanvim handle everything automatically
+      }
+
+      vim.keymap.set("n", "<space>k", "<Cmd>RustLsp hover actions<CR>", { noremap = true, silent = true })
+    end,
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+
+  -- Debug Adapter Protocol (DAP) Configuration
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      -- DAP configuration without keymaps (keymaps are in mappings.lua)
+      local dap = require "dap"
+      -- Additional DAP setup can go here if needed
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+
+  -- Code Structure and Documentation Navigation
+  {
+    "stevearc/aerial.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("aerial").setup {
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '[' and ']'
+          vim.keymap.set("n", "]a", "<cmd>AerialNext<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "[a", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        end,
+      }
+      -- Keymap to toggle aerial
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    end,
+    lazy = false,
+  },
+
+  -- Git GUI
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
+  -- Navigation and Preview Plugins
+  {
+    "nvim-neotest/nvim-nio",
+    lazy = false,
+  },
+  {
+    "rmagatti/goto-preview",
+    lazy = false,
+    config = function()
+      require("goto-preview").setup {
+        default_mappings = true,
+      }
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  
+  -- ## Jisang Setup End## --
+
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
