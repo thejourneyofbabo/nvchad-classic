6ac98b8c286f6fbb26b6d3c167623aea471ad7fd
JisangYun <journeyofbabo@gmail.com>
2025-10-03 02:10:22 +0000

    add: clipboard options (osc52 for docker, ssh)

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,16 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- OSC 52 clipboard for Docker/SSH environments
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
