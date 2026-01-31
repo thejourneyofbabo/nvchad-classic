2dc65e13d92085778af33b525ccb43e3bf18b413
JisangYun <journeyofbabo@gmail.com>
2026-01-31 17:54:28 +0900

    Add: Lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,4 +25,24 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  -- Git GUI
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
 }
